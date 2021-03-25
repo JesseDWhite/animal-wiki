d74a3561a38c291ca41635a9b2f638f0163efa1a
Jesse White <jesse.white6@gmail.com>
2021-03-25 11:04:31 -0700

    add four animals to home page and begin java hide functions.
    co-authored-by: Brandon Magofna <Magofna68@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+//     $("#reptile").click(function () {
+//         $(".reptile-showing").toggle();
+//         $(".reptile-hidden").toggle();
+//     });
+//     $("#mammal").click(function () {
+//         $(".mammal-showing").toggle();
+//         $(".mammal-hidden").toggle();
+//     });
+//     $("#amphibians").click(function () {
+//         $(".amphibian-showing").toggle();
+//         $(".amphibian-hidden").toggle();
+//     });
+//     $("#insects").click(function () {
+//         $(".insects-showing").toggle();
+//         $(".insects-hidden").toggle();
+//     });
+// });
+})
